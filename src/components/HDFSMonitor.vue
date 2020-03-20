833afb10a0bd9b8f6f44045645ae71b79eac6551
zhy <zhuhanyu1@gmail.com>
2020-03-20 23:38:14 +0800

    安装echarts

---
+++ src/components/HDFSMonitor.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="hdfs-monitor">
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'HDFSMonitor'
+}
+</script>
---
+++ src/components/NodeMonitor.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="node-monitor">
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'NodeMonitor'
+}
+</script>
